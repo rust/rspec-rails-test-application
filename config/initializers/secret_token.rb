2e22a3ef1722f4cd147af24a3245bae57874b017
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-09-01 22:29:34 +0900

    Initial
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecRailsReproduce::Application.config.secret_token = 'd186793ab692f12030937a5d34516fd99fb81739a5d02ea609139175cc40889f8de7cb1930c772533f4682e4d4dad84ff41f149d8b8e6c5bcd5c13e3780293cb'
